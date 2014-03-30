4cf927f1d993847cd43d5222b5639abd2987429a
epait <ericpait@gmail.com>
2014-03-30 04:46:12 -0400

    trying to get natural keys working

--- county_info/admin.py
+++ county_info/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from county_info.models import District
+from county_info.models import District, Graduation
 
 # Register your models here.
 class DistrictAdmin(admin.ModelAdmin):
 	search_fields = ('lea_name', 'lea_city')
 
 admin.site.register(District, DistrictAdmin)
+
+class GraduationAdmin(admin.ModelAdmin):
+	search_fields = ('code',)
+
+admin.site.register(Graduation, GraduationAdmin)
--- county_info/models.py
+++ county_info/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class DistrictManager(models.Manager):
+    def get_by_natural_key(self, lea_code, lea_school_year):
+        return self.get(lea_code=lea_code, lea_school_year=lea_school_year)
 
 class District(models.Model):
+	objects = DistrictManager()
+
 	lea_code = models.CharField(max_length=4, verbose_name="Code")
 	lea_name = models.CharField(max_length=150, verbose_name="Name")
 	lea_city = models.CharField(max_length=150, verbose_name="City")
@@ -17,6 +22,34 @@ class District(models.Model):
 		verbose_name = "School District"
 		verbose_name_plural = "School Districts"
 		ordering = ('lea_name', 'lea_school_year')
+		unique_together = (('lea_code', 'lea_school_year'),)
 
 	def __unicode__(self):
 		return U'%s (%s)' %(self.lea_name, self.lea_school_year)
+
+	def natural_key(self):
+		return (self.lea_code, self.lea_school_year)
+
+class Graduation(models.Model):
+	# code = models.CharField(max_length=4, verbose_name="Code")
+	# school_year = models.CharField(max_length=10, verbose_name="School Year")
+	district = models.ForeignKey('District')
+	graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	female_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	male_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	native_american_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	asian_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	black_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	hispanic_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	multiracial_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	white_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	freelunch_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	notfreelunch_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	
+
+	class Meta(object):
+		verbose_name = "District Graduation Rate"
+		verbose_name_plural = "District Graduation Rates"
+
+	def __unicode__(self):
+		return U'%s (%s)' %(district.lea_name, district.lea_school_year)
