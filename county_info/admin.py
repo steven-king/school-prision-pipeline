09b4607331ea0e6cf615b5cb9c826b5435f04c2b
epait <ericpait@gmail.com>
2014-03-31 01:34:41 -0400

    discipline data added

--- county_info/admin.py
+++ county_info/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from county_info.models import District, Graduation
+from county_info.models import District, Graduation, DisciplineRate
 
 # Register your models here.
 class DistrictAdmin(admin.ModelAdmin):
@@ -10,6 +10,12 @@ admin.site.register(District, DistrictAdmin)
 
 class GraduationAdmin(admin.ModelAdmin):
 	list_display = ('district', 'school_year', 'graduation_rate', 'female_graduation_rate', 'male_graduation_rate', 'freelunch_graduation_rate', 'black_graduation_rate', 'hispanic_graduation_rate')
-	search_fields = ('code',)
+	search_fields = ('district', 'school_year')
 
 admin.site.register(Graduation, GraduationAdmin)
+
+class DisciplineRateAdmin(admin.ModelAdmin):
+	list_display = ('district', 'school_year', 'category', 'short_suspensions', 'long_suspensions', 'expulsions', 'crime')
+	search_fields = ('district', 'school_year')
+
+admin.site.register(DisciplineRate, DisciplineRateAdmin)
--- county_info/models.py
+++ county_info/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# class DistrictManager(models.Manager):
-    # def get_by_natural_key(self, lea_code, lea_school_year):
-        # return self.get(lea_code=lea_code, lea_school_year=lea_school_year)
-
 class District(models.Model):
-	# objects = DistrictManager()
-
 	lea_code = models.CharField(max_length=3, verbose_name="Code", primary_key=True)
 	lea_name = models.CharField(max_length=150, verbose_name="Name")
 	lea_city = models.CharField(max_length=150, verbose_name="City")
 	lea_state = models.CharField(max_length=150, verbose_name="State")
-	# lea_school_year = models.CharField(max_length=10, verbose_name="School Year")
 	lea_type = models.CharField(max_length=1, verbose_name="Type")
 	lea_email = models.CharField(max_length=100, null=True, verbose_name="Email")
 	lea_superintendent = models.CharField(max_length=100, null=True, verbose_name="Superintendent")
@@ -22,18 +15,14 @@ class District(models.Model):
 		verbose_name = "School District"
 		verbose_name_plural = "School Districts"
 		ordering = ('lea_name',)
-		# unique_together = (('lea_code', 'lea_school_year'),)
 
 	def __unicode__(self):
 		return U'%s (%s)' %(self.lea_name, self.lea_code)
 
-	# def natural_key(self):
-		# return (self.lea_code, self.lea_school_year)
 
 class Graduation(models.Model):
 	district = models.ForeignKey('District')
 	school_year = models.CharField(max_length=10, verbose_name="School Year")
-	# district = models.ForeignKey('District')
 	graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
 	female_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
 	male_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
@@ -46,7 +35,6 @@ class Graduation(models.Model):
 	freelunch_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
 	notfreelunch_graduation_rate = models.DecimalField(max_digits=6, decimal_places=5, null=True)
 	
-
 	class Meta(object):
 		verbose_name = "Graduation Rates"
 		verbose_name_plural = "Graduation Rates"
@@ -54,3 +42,35 @@ class Graduation(models.Model):
 
 	def __unicode__(self):
 		return U'%s (%s)' %(self.district.lea_name, self.school_year)
+
+
+class DisciplineRate(models.Model):
+	district = models.ForeignKey('District')
+	school_year = models.CharField(max_length=10, verbose_name="School Year")
+	category = models.CharField(max_length=1)
+	school_improvement_percent = models.DecimalField(max_digits=6, decimal_places=5, null=True)
+	short_suspensions = models.IntegerField(max_length=4, null=True, help_text="Number Per 1000 Students")
+	long_suspensions = models.IntegerField(max_length=4, null=True, help_text="Number Per 1000 Students")
+	expulsions = models.IntegerField(max_length=4, null=True, help_text="Number Per 1000 Students")
+	crime = models.IntegerField(max_length=4, null=True, help_text="Number Per 1000 Students")
+
+	class Meta(object):
+		verbose_name = "Discipline Rates"
+		verbose_name_plural = "Discipline Rates"
+		ordering = ('district__lea_name', '-school_year')
+
+	def __unicode__(self):
+		return U'%s (%s) - %s' %(self.district.lea_name, self.school_year, self.category)
+
+
+class Demographics(models.Model):
+	district = models.ForeignKey('District')
+	school_year = models.CharField(max_length=10, verbose_name="School Year")
+
+	class Meta(object):
+		verbose_name_plural = "Demographics"
+		ordering = ('district__lea_name', '-school_year')
+
+	def __unicode__(self):
+		return U'%s (%s) - %s' %(self.district.lea_name, self.school_year, self.category)
+
