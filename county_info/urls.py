058cca3518b827028abb91a27a1e98539d3e172e
epait <ericpait@gmail.com>
2014-04-05 22:21:36 -0400

    setup urls.py

---
+++ county_info/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # url(r'^$', 'school_prison_pipeline.views.home', name='home'),
+)
--- school_prison_pipeline/urls.py
+++ school_prison_pipeline/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^', include('county_info.urls')),
+    url(r'^', include('county_info.urls')),
 )
