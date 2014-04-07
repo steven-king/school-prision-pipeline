1d45eafe7fe32f12a72b72ae9c95c2fbfb0bf2ae
epait <ericpait@gmail.com>
2014-04-06 23:36:14 -0400

    update settings.py

--- school_prison_pipeline/settings.py
+++ school_prison_pipeline/settings.py
@@ -10,23 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-# from settings_secret import *
+from settings_secret import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'school-discipline.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
