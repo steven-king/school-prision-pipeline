0fd62a88351ed5e968080759e2724ecb9c790cc7
mclawges22 <mclawges22@gmail.com>
2014-04-06 23:16:04 -0400

    changing things so I can localhost and basic css fonts and things

--- county_info/views.py
+++ county_info/views.py
@@ -5,21 +5,21 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def home(request):
 	context = {
-		'northcarolina': State.objects.get(state_name="North Carolina"),
-		'districts': District.objects.all(),
-		'graduation_rates': Graduation.objects.all(),
-		'discipline_rates': DisciplineRate.objects.all(),
-		'demographics': Demographics.objects.all(),
-		'attendance_rates': Attendance.objects.all(),
-		'gradelevel_rates': GradeLevel.objects.all(),
-		'specialcourse_rates': SpecialCourses.objects.all(),
-		'freelunch_rates': FreeLunch.objects.all(),
-		'stategraduation_rates': StateGraduation.objects.all(),
-		'statediscipline_rates': StateDiscipline.objects.all(),
-		'statedemographics': StateDemographics.objects.all(),
-		'stateattendance_rates': StateAttendance.objects.all(),
-		'stategradelevel_rates': StateGradeLevel.objects.all(),
-		'statespecialcourse_rates': StateSpecialCourses.objects.all(),
+		#'northcarolina': State.objects.get(state_name="North Carolina"),
+		#'districts': District.objects.all(),
+		#'graduation_rates': Graduation.objects.all(),
+		#'discipline_rates': DisciplineRate.objects.all(),
+		#'demographics': Demographics.objects.all(),
+		#'attendance_rates': Attendance.objects.all(),
+		#'gradelevel_rates': GradeLevel.objects.all(),
+		#'specialcourse_rates': SpecialCourses.objects.all(),
+		#'freelunch_rates': FreeLunch.objects.all(),
+		#'stategraduation_rates': StateGraduation.objects.all(),
+		#'statediscipline_rates': StateDiscipline.objects.all(),
+		#'statedemographics': StateDemographics.objects.all(),
+		#'stateattendance_rates': StateAttendance.objects.all(),
+		#'stategradelevel_rates': StateGradeLevel.objects.all(),
+		#'statespecialcourse_rates': StateSpecialCourses.objects.all(),
 	}
 	return render(request, "county_info/home.html", context)
 
