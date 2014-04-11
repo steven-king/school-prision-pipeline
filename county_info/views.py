f68832d8c80197975ce85e6c9cad153f9303c3cd
mclawges22 <mclawges22@gmail.com>
2014-04-11 12:49:26 -0400

    basic compare tab CSS

--- county_info/views.py
+++ county_info/views.py
@@ -1,7 +1,7 @@
 from county_info.models import District, Graduation, DisciplineRate, Demographics, Attendance, GradeLevel, SpecialCourses, FreeLunch, State, StateDemographics, StateGraduation, StateAttendance, StateGradeLevel, StateSpecialCourses, StateDiscipline
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-import ipdb
+
 
 # Create your views here.
 def home(request):
