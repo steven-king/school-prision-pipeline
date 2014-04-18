2de8966fb6131743c1ab6d64efd7e368966e2445
epait <ericpait@gmail.com>
2014-04-18 16:21:55 -0400

    add graduation rates to API

--- county_info/serializers.py
+++ county_info/serializers.py
@@ -8,5 +8,4 @@ class GraduationSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Graduation
-		fields = ('district', 'school_year', 'graduation_rate', 'native_american_graduation_rate', 'asian_graduation_rate', 'black_graduation_rate', 'hispanic_graduation_rate', 'multiracial_graduation_rate', 'white_graduation_rate', 
-			'freelunch_graduation_rate')
+		fields = ('district', 'school_year', 'graduation_rate')
