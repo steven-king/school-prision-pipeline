4cffa1f13bb583ea0a4fa8867822f48a6efc27a4
epait <ericpait@gmail.com>
2014-04-20 14:33:43 -0400

    district profile api

--- county_info/serializers.py
+++ county_info/serializers.py
@@ -9,6 +9,10 @@ class GraduationSerializer(serializers.ModelSerializer):
 		model = Graduation
 		fields = ('district', 'school_year', 'graduation_rate')
 
+class GraduationDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Graduation
+		fields = ('school_year', 'graduation_rate', 'freelunch_graduation_rate')
 
 class SatScoreSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -20,17 +24,40 @@ class FreeLunchSerializer(serializers.ModelSerializer):
 		model = FreeLunch
 		fields = ('district', 'school_year', 'percent_needy')
 
+class FreeLunchDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = FreeLunch
+		fields = ('school_year', 'percent_needy')
+
 class DisciplineRateSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DisciplineRate
 		fields = ('district', 'school_year', 'category', 'composite_rate')
+
+class DisciplineRateDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = DisciplineRate
+		fields = ('school_year', 'category', 'composite_rate')
 
 class ExpensesSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Demographics
 		fields = ('district', 'school_year', 'expenses_per_pupil')
 
+class DemographicsDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Demographics
+		fields = ('school_year', 'expenses_per_pupil', 'sat_participation', 'sat_average_score', 'percent_native_american','percent_asian', 'percent_black', 'percent_hispanic', 'percent_white', 'percent_multiracial', 'percent_pacific_islander')
+
 class DistrictSerializer(serializers.ModelSerializer):
+	graduation_rates = GraduationDetailSerializer(many=True)
+	demographics = DemographicsDetailSerializer(many=True)
+	discipline_rates = DisciplineRateDetailSerializer(many=True)
+	freelunch_rates = FreeLunchDetailSerializer(many=True)
+
 	class Meta:
 		model = District
-		fields = ('lea_name', 'lea_code')
+		fields = ('lea_name', 'lea_code', 'graduation_rates', 'demographics', 'discipline_rates', 'freelunch_rates')
+
+
+
