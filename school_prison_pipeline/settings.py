377306526a20c020116b11484761ae9b1c09c52a
epait <ericpait@gmail.com>
2014-04-24 01:24:37 -0400

    mobile sticky dropdowns to heroku

--- school_prison_pipeline/settings.py
+++ school_prison_pipeline/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from settings_secret import *
+# from settings_secret import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -107,27 +107,27 @@ TEMPLATE_DIRS = (
 
 
 
-# DATABASES = {}
+DATABASES = {}
 
-# # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-# # Static asset configuration
-# import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static_media'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_media'),
+)
 
 
 
